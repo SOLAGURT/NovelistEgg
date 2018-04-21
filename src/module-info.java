5ceeaa480c094377de02e564470198062755b989
SOLAGURT <icespear1022@gmail.com>
2018-04-21 12:19:05 +0900

    Language level 8 -> 9 add module-info

---
+++ src/module-info.java
@@ -0,0 +1,17 @@
+module main.com.ensemblebox.novelistegg {
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires kotlin.stdlib;
+    requires java.base;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    //requires javafx.graphics;
+    requires javafx.web;
+    //requires jdk.incubator.httpclient;
+
+    exports main.com.ensemblebox.novelistegg;
+
+}
