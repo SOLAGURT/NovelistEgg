93b756043de13071bb68545cb02cb653b8615b9e
SOLAGURT <icespear1022@gmail.com>
2018-04-21 12:16:57 +0900

    create HttpConnect

---
+++ src/main/com/ensemblebox/novelistegg/HttpConnect.kt
@@ -0,0 +1,19 @@
+package main.com.ensemblebox.novelistegg
+
+/**
+ * Created by ArcenCiel on 2018/04/17.
+ */
+
+class HttpConnect(address: String) {
+
+    var html_extract: StringBuilder = StringBuilder(30000)
+
+    //html取得処理
+    init {
+
+
+
+    }
+
+
+}
